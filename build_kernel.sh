f7ee73994eff10336d8db12c0e95005710cc2f50
Kristoffer Grundström <lovaren@gmail.com>
2024-04-15 19:26:58 +0200

    Reuploaded kernel source code

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-export ARCH=arm64
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
-export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
-
-make ARCH=arm64 exynos2100-o1sxxx_defconfig
-make ARCH=arm64 -j16
+version https://git-lfs.github.com/spec/v1
+oid sha256:b596b9ca65e7f76fc1d154659d9c27039b6e1e5b2a333a1931e94ba78c7c885c
+size 196
